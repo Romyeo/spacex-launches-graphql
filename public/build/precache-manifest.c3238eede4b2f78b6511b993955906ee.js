90d21b437ec701dd7edbc75e2d375d1e5e1ed177
Romyeo <vincent.nocum@gmail.com>
2019-10-03 11:22:21 +0800

    Fix: wrong uri, 'grahpql' -> '/graphql'

---
+++ public/build/precache-manifest.c3238eede4b2f78b6511b993955906ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5d05f2a7db44bb709297c8d0fec841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f67ba31455e4f809be4",
+    "url": "/static/css/main.a003a13d.chunk.css"
+  },
+  {
+    "revision": "ebafc48b766735267173",
+    "url": "/static/js/2.f2b4f76c.chunk.js"
+  },
+  {
+    "revision": "3f67ba31455e4f809be4",
+    "url": "/static/js/main.80ed9fa1.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  },
+  {
+    "revision": "e01e7c4ec0a14363a33e6ffeed22518b",
+    "url": "/static/media/logo.e01e7c4e.svg"
+  },
+  {
+    "revision": "f427bb7a3cca35da3f8a05d47245e8d2",
+    "url": "/static/media/rocket.f427bb7a.png"
+  }
+]);
